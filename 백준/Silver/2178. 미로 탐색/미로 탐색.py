a51b6b4c49b5ac2f139641182f9d4b8cfca99d39
최 우 성 <perfumemakes@gmail.com>
2022-10-15 21:28:25 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 32460 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+n,m = map(int, input().split())
+array = [ [] * m ] * n
+
+for i in range(n):
+    array[i] = list(map(int, sys.stdin.readline().rstrip()))
+
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+
+def bfs(x,y):
+    queue = deque()
+    queue.append((x,y))
+    
+    while queue:
+        x,y = queue.popleft()
+        
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+            if nx < 0 or ny < 0 or nx >= n or ny >= m:
+                continue
+            if array[nx][ny] == 0:
+                continue
+            
+            if array[nx][ny] == 1:
+                array[nx][ny] = array[x][y] + 1
+                queue.append((nx,ny))
+    
+    return array[n-1][m-1]
+
+print(bfs(0,0))
