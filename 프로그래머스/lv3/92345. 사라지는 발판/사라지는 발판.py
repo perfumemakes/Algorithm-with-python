5e1840ddb77495ea82ad52a1ee282d49f001fcec
최 우 성 <perfumemakes@gmail.com>
2022-11-29 01:31:10 +0900

    [level 3] Title: 사라지는 발판, Time: 0.01 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/92345. 사라지는 발판/사라지는 발판.py
+++ 프로그래머스/lv3/92345. 사라지는 발판/사라지는 발판.py
@@ -1,47 +1,58 @@
+dir = ((-1,0),(0,1),(1,0),(0,-1))
 
-from copy import deepcopy
-
-def check_move(board, loc):
-    r, c = loc
-    for idx, (dr, dc) in enumerate([[0,1], [0, -1],  [1, 0], [-1, 0]]):
-        nr, nc = r +dr, c +dc
-        if ((0 <= nr < len(board)) and (0 <= nc < len(board[0])) and board[nr][nc] == 1):
-            return True
-        
-    return False
-
-def search(board, aloc, bloc, step):
-    r,  c = aloc if step%2 == 0 else bloc
-    if check_move(board, [r,c]) == False:
-        return (False, 0)
-    if aloc==bloc:
-        return (True, 1)
-    
-    nboard = deepcopy(board)
-    nboard[r][c] = 0
+def A_turn(ar,ac,br,bc,cnt,board):
+    if board[ar][ac] == 0:
+        return (1,cnt)
+    winner = []
+    loser = []
     flag = False
-    max_turn, min_turn = 0, float('inf')
-    for idx, (dr, dc) in enumerate([[0,1], [0, -1], [1, 0], [-1, 0]]):
-        nr, nc = r + dr, c + dc
-        if ((0 <= nr < len(board)) and (0 <= nc < len(board[0])) and board[nr][nc] == 1):
-            if step%2 == 0:
-                ret = search(nboard, [nr, nc], bloc, step+1)
+    for dr,dc in dir:
+        nr,nc = ar+dr,ac+dc
+        if 0<=nr<len(board) and 0<=nc<len(board[0]) and board[nr][nc] == 1:
+            flag = True
+            temp = [row[:] for row in board]
+            temp[ar][ac] = 0
+            iswin,turn = B_turn(br,bc,nr,nc,cnt+1,temp)
+            if iswin:
+                winner.append(turn)
             else:
-                ret = search(nboard, aloc, [nr, nc], step+1)
-                
-            if ret[0]==False:
-                flag = True
-                min_turn = min(min_turn, ret[1])
-            else:
-                max_turn = max(max_turn, ret[1])
-                
-    if flag == True:
-        return (flag, min_turn+1)
-    
+                loser.append(turn)
+    if flag:
+        if winner:
+            return (0,min(winner))
+        else:
+            return (1,max(loser))
     else:
-        return (flag, max_turn+1)
-    
+        return (1,cnt)
+
+
+def B_turn(br,bc,ar,ac,cnt,board):
+    if board[br][bc] == 0:
+        return (1,cnt)
+    winner = []
+    loser = []
+    flag = False
+    for dr,dc in dir:
+        nr,nc = br+dr,bc+dc
+        if 0<=nr<len(board) and 0<=nc<len(board[0]) and board[nr][nc] == 1:
+            flag = True
+            temp = [row[:] for row in board]
+            temp[br][bc] = 0
+            iswin,turn = A_turn(ar,ac,nr,nc,cnt+1,temp)
+            if iswin:
+                winner.append(turn)
+            else:
+                loser.append(turn)
+    if flag:
+        if winner:
+            return (0,min(winner))
+        else:
+            return (1,max(loser))
+    else:
+        return (1,cnt)
+
+
 def solution(board, aloc, bloc):
-    answer = search(board, aloc, bloc, 0)
-    return answer[1]
-                
+    ar,ac,br,bc = aloc[0],aloc[1],bloc[0],bloc[1]
+    answer = A_turn(ar,ac,br,bc,0,board)[1]
+    return answer
