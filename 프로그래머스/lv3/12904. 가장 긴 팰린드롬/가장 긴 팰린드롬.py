1551e1d30062f0a372f194f34ee152245334b2c3
최 우 성 <perfumemakes@gmail.com>
2022-12-09 23:06:08 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 251.04 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -0,0 +1,16 @@
+def solution(s:str)->str:
+    def expand(left: int, right: int)->str:
+        while left >= 0 and right <= len(s) and s[left] == s[right -1]:
+            left -= 1
+            right += 1
+        return s[left + 1: right - 1]
+    
+    if len(s) < 2 or s == s[::-1]:
+        return len(s)
+    
+    result = ''
+    for i in range(len(s)-1):
+        result = max(result, expand(i, i+1), expand(i, i+2), key=len)
+        
+
+    return len(result)
