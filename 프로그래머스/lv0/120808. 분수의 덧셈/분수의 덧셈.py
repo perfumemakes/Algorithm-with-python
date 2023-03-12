929f297889a8f157bf30689aa862b0c8ac899a9d
최 우 성 <perfumemakes@gmail.com>
2023-03-12 23:16:51 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -4,17 +4,12 @@ def gcd(x,y):
     return x
     
 def solution(numer1, denom1, numer2, denom2):
-    
-    for i in range(max(denom1, denom2), denom1*denom2 +1):
-        if i % denom1 == 0 and i % denom2 == 0:
-            target = i
-            break
             
-    numer1 = numer1 * (target//denom1)
-    numer2 = numer2 * (target//denom2)
+    numer1 = numer1 * denom2
+    numer2 = numer2 * denom1
     
     numer = numer1 + numer2
-    denom = target
+    denom = denom1 * denom2
     
     x = max(numer, denom)
     y = min(numer, denom)
