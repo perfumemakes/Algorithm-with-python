a5cca56b2c8e029fd2e74aa548528fbab9dc95b7
최 우 성 <perfumemakes@gmail.com>
2023-03-30 23:25:38 +0900

    [level 2] Title: 타겟 넘버, Time: 9.98 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,4 +1,28 @@
-def solution(numbers, target):
+def recur(numbers, target, idx, empty_list, cnt):
+        if idx == len(numbers):
+            if sum(empty_list) == target:
+                cnt.append(1)
+            return
+        empty_list.append(-int(numbers[idx]))
+        #print(empty_list)
+        idx += 1
+        recur(numbers, target, idx, empty_list, cnt)
+        idx -= 1
+        empty_list.pop()
+        empty_list.append(int(numbers[idx]))
+        #print(empty_list)
+        idx += 1
+        recur(numbers, target, idx, empty_list, cnt)
+        idx -= 1
+        empty_list.pop()
+        
+def solution(numbers, target) -> int:
+    empty_list = []
+    cnt = []
+    recur(numbers, target, 0, empty_list, cnt)
+    return sum(cnt)
+
+def solution2(numbers, target):
     
     start=[0]
     for i in numbers:
