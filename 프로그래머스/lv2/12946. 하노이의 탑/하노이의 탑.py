8757bd151b6fe97917f8533102b0ddc3aa0f2f83
최 우 성 <perfumemakes@gmail.com>
2023-04-08 22:00:37 +0900

    [level 2] Title: 하노이의 탑, Time: 8.46 ms, Memory: 18.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.py
@@ -1,7 +1,9 @@
 def hanoi(n, start, mid, end, answer):
     if n==1:
+        #print(f'원판 1을 {start}에서 {end}로 옮긴다')
         return answer.append([start, end])
     hanoi(n-1, start, end, mid, answer)
+    #print(f'원판 {n}을 {start}에서 {end}로 옮긴다')
     answer.append([start, end])
     hanoi(n-1, mid, start, end, answer)
     
@@ -10,3 +12,6 @@ def solution(n):
     answer = []
     hanoi(n, 1, 2, 3, answer)
     return answer
+
+if __name__=="__main__":
+    solution(3)
